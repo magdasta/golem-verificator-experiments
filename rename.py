8c5e279130e1c0a2e38758b16fdf01a237533c26
Magda Stasiewicz <magdalena.stasiewicz@golem.network>
2018-05-25 11:21:05 +0200

    Script for removing unnecessary data from file names (resolution info and serial number)

---
+++ rename.py
@@ -0,0 +1,24 @@
+'''
+Removes resolution and serial number from file names
+'''
+import os
+from sys import argv
+
+
+def simplify_file_name(file_name):
+    split = file_name.split("_")
+    samples_and_extension = split[-1]
+    base_name = split[0]
+    return base_name + "_" + samples_and_extension
+
+
+def rename():
+    source_images_directory = argv[1]
+    for root, dirs, files in os.walk(source_images_directory):
+        for file_name in files:
+            new_file_name = simplify_file_name(file_name)
+            os.rename(os.path.join(root, file_name), os.path.join(root, new_file_name))
+
+
+if __name__ == "__main__":
+    rename()
