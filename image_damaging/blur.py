7551d73e84d4044a2f2d7d808041dfd0230a8ee9
Witek <nieznany.sprawiciel@gmail.com>
2018-05-28 10:35:35 +0200

    removed function

--- image_damaging/blur.py
+++ image_damaging/blur.py
@@ -15,23 +15,6 @@ def blur( image, parameters ):
       
 ## ======================= ##
 ##
-def blur_images( src_file, target_dir ):
-    
-    [ file_name, extension ] = os.path.splitext( os.path.basename( src_file ) ) 
-    
-    for blur_size in range( 1, 5 ):
-        target_file = file_name + "_blured_" + str( blur_size ) + extension
-        target_path = os.path.join( target_dir, target_file )
-        
-        print( "    Generating image: [" + target_path + "] blured with kernel_size: " + str( blur_size ) )
-        
-        parameters = helpers.Parameters()
-        parameters.kernel_size = blur_size
-        
-        helpers.process_image( src_file, target_path, blur, parameters )
-      
-## ======================= ##
-##
 def run():
 
     parameters = []
