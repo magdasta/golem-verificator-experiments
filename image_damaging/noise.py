73fe967c87efebe16e37be437f048e79d185ecb3
Witek <nieznany.sprawiciel@gmail.com>
2018-05-28 14:33:39 +0200

    noise improvement

--- image_damaging/noise.py
+++ image_damaging/noise.py
@@ -1,4 +1,4 @@
-from PIL import Image, ImageFilter
+from PIL import Image, ImageMath
 import sys
 import os
 import helpers
@@ -6,8 +6,17 @@ import numpy
 
 
 
+## ======================= ##
+##
+def add_noise( image, noise ):
 
-
+    ( image_r, image_g, image_b ) = image.split()
+    
+    noised_r = ImageMath.eval( "convert( a+b, 'L' )", a=image_r, b=noise )
+    noised_g = ImageMath.eval( "convert( a+b, 'L' )", a=image_g, b=noise )
+    noised_b = ImageMath.eval( "convert( a+b, 'L' )", a=image_b, b=noise )
+    
+    return Image.merge( 'RGB', ( noised_r, noised_g, noised_b ) )
 
 ## ======================= ##
 ##
@@ -19,25 +28,34 @@ def noise( image, parameters ):
     
     random_noise = numpy.random.normal( mean, stddev, size )
     random_noise = numpy.reshape( random_noise, [ image.height, image.width ] )
-    
-    random_image = Image.fromarray( random_noise.astype( 'uint8' ) )
-    random_image = Image.merge( 'RGB', ( random_image, random_image, random_image ) )
-    
-    return Image.blend( image, random_image, 0.1 )
 
+    random_image = Image.fromarray( random_noise )
+
+    return add_noise( image, random_image )
+
+      
+## ======================= ##
+##
+def param_to_postfix( parameters ):
+    
+    return "_noise_mean" + str( parameters.mean ) + "_stddev" + str( parameters.stddev )
       
 ## ======================= ##
 ##
 def run():
 
     parameters = []
-    parameters_set = helpers.Parameters()
     
-    parameters_set.mean = 0
-    parameters_set.stddev = 5
-    parameters_set.file_postfix = "_noise_mean0_stddev5"
+    stddevs = ( 3, 6, 9, 12, 15 )
     
-    parameters.append( parameters_set )
+    for stddev in stddevs:
+        parameters_set = helpers.Parameters()
+        
+        parameters_set.mean = 0
+        parameters_set.stddev = stddev
+        parameters_set.file_postfix = param_to_postfix( parameters_set )
+        
+        parameters.append( parameters_set )
 
     helpers.simple_process_directory( sys.argv[ 1 ], sys.argv[ 2 ], noise, parameters )
 
