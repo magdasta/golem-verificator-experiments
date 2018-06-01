1d61993745aa8d97bea027e8e2fd3702610f5054
Witek <nieznany.sprawiciel@gmail.com>
2018-06-01 15:07:50 +0200

    CSV creation

--- classification/extract_features.py
+++ classification/extract_features.py
@@ -2,6 +2,7 @@ import extract_params as extr
 import list_comparisions
 
 import sys
+import csv
 
 
 ## ======================= ##
@@ -19,11 +20,45 @@ def unique_params( compare_list ):
     return labels
 
     
+## ======================= ##
+## 
+def compare_images( reference_dir, compare_dir_parent, csv_file ):
+    
+    compare_list = list_comparisions.list_all( reference_dir, compare_dir_parent )
+    
+    params = list( unique_params( compare_list ) )
+    labels = list()
+
+    labels.append( "reference_image" )
+    labels.append( "image" )
+    
+    labels = labels + params
+    
+    print( "Parameters that will be written to csv file: " + str( params ) )
+
+    with open( csv_file, 'w', newline='') as csvfile:
+
+        writer = csv.DictWriter( csvfile, fieldnames = labels )
+        writer.writeheader()
+        
+        for ( reference, to_compare ) in compare_list:
+            
+            paramsDict = dict()
+            paramsDict[ "reference_image" ] = reference
+            paramsDict[ "image" ] = to_compare
+            
+            params_list = extr.extract_params( to_compare )
+            
+            for param in params_list:
+                paramsDict[ param[ 0 ] ] = param[ 1 ]
+        
+            writer.writerow( paramsDict )
+    
     
 reference_dir = sys.argv[ 1 ]
 compare_dir_parent = sys.argv[ 2 ]
+csv_file = sys.argv[ 3 ]
 
-compare_list = list_comparisions.list_all( reference_dir, compare_dir_parent )
-params = unique_params( compare_list )
-
-print( params )
+compare_images( reference_dir, compare_dir_parent, csv_file )
+        
+    
