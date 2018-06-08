5569d0b57f59294999f37993800dcee036a633f5
Witek <nieznany.sprawiciel@gmail.com>
2018-06-08 14:25:37 +0200

    Edges generator

---
+++ image_damaging/edges.py
@@ -0,0 +1,31 @@
+from PIL import Image, ImageFilter
+import sys
+import os
+import helpers
+
+
+## ======================= ##
+##
+def find_edges( image, parameters ):
+
+    return image.filter( ImageFilter.FIND_EDGES )
+    
+
+## ======================= ##
+##
+def run():
+
+    parameters = []
+    parameters_set = helpers.Parameters()
+    parameters_set.file_postfix = "_[edged]"
+    
+    parameters.append( parameters_set )
+
+    helpers.simple_process_directory( sys.argv[1], sys.argv[2], find_edges, parameters )
+    
+## ======================= ##
+##
+if __name__ == "__main__":
+    run()
+    
+    
