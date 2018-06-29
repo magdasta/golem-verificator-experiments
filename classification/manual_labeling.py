9c1239c65a50fd33b987f53fb3c0c320f00966a8
Witek <nieznany.sprawiciel@gmail.com>
2018-06-29 12:13:11 +0200

    Loading labeling from dataset

--- classification/manual_labeling.py
+++ classification/manual_labeling.py
@@ -4,13 +4,30 @@ import math
 
 import sys
 
+import loading
+
+
 
 ## ======================= ##
 ##
 tiles = 10
 selected_crops = numpy.zeros( ( tiles, tiles ), numpy.uint8 )
+
+TrueLabel = 0
+FalseLabel = 1
+DontKnowLabel = 2
+IgnoreLabel = 3
+
+TrueColor = ( 0, 255, 0 )
+FalseColor = ( 255, 0, 0 )
+DontKnowColor = ( 0, 200, 200 )
+IgnoreColor = ( 0, 0, 255 )
+
+# Images
 screen = None
 mask = None
+
+# Configuration
 show_mask =  True
 
 
@@ -75,11 +92,16 @@ def apply_mask( image, selected_crops ):
     for tile_x in range( 0, selected_crops.shape[ 0 ] ):
         for tile_y in range( 0, selected_crops.shape[ 1 ] ):
         
-            if selected_crops[ tile_x ][ tile_y ]:
-                
-                box = crop_to_box( tile_x, tile_y )
-                cv2.rectangle( mask, ( box[ 0 ], box[ 1 ] ), ( box[ 2 ], box[ 3 ] ), ( 255, 0, 0 ), thickness = cv2.FILLED )
-                
+            box = crop_to_box( tile_x, tile_y )
+            
+            if selected_crops[ tile_x ][ tile_y ] == FalseLabel:
+                cv2.rectangle( mask, ( box[ 0 ], box[ 1 ] ), ( box[ 2 ], box[ 3 ] ), FalseColor, thickness = cv2.FILLED )
+            elif selected_crops[ tile_x ][ tile_y ] == TrueLabel:
+                cv2.rectangle( mask, ( box[ 0 ], box[ 1 ] ), ( box[ 2 ], box[ 3 ] ), TrueColor, thickness = cv2.FILLED )
+            elif selected_crops[ tile_x ][ tile_y ] == DontKnowLabel:
+                cv2.rectangle( mask, ( box[ 0 ], box[ 1 ] ), ( box[ 2 ], box[ 3 ] ), DontKnowColor, thickness = cv2.FILLED )
+            elif selected_crops[ tile_x ][ tile_y ] == IgnoreLabel:
+                cv2.rectangle( mask, ( box[ 0 ], box[ 1 ] ), ( box[ 2 ], box[ 3 ] ), IgnoreColor, thickness = cv2.FILLED )
     
     alpha = 0.6
     
@@ -94,14 +116,64 @@ def apply_mask( image, selected_crops ):
 def show_hide_mask():
     global show_mask
     show_mask = not show_mask
+
+
+## ======================= ##
+##
+def get_label( row ):
+    
+    label = row[ "label" ]
+    if label == b"TRUE":
+        return TrueLabel
+    elif label == b"FALSE":
+        return FalseLabel
+    elif label == b"DONT_KNOW":
+        return DontKnowLabel
+    else:
+        return IgnoreLabel
+
+    
+## ======================= ##
+##
+def fill_crops_selections( crop_rows ):
+    global selected_crops
+
+    selected_crops = numpy.zeros( ( tiles, tiles ), numpy.uint8 )
+
+    for row in crop_rows:
+        
+        tile_x = row[ "crop_x" ]
+        tile_y = row[ "crop_y" ]
+        
+        selected_crops[ tile_x ][ tile_y ] = get_label( row )
+        
+    
+## ======================= ##
+##
+def load_row( data, row ):
+    
+    print( "Selected comparision:" )
+    print( "    " + row[ "reference_image" ].decode('UTF-8')  )
+    print( "    " + row[ "image" ].decode('UTF-8')  )
+    
+    # Find all crops rows
+    crop_rows = data[ ( data[ "reference_image" ] == row[ "reference_image" ] ) & ( data[ "image" ] == row[ "image" ] ) ]
+    
+    fill_crops_selections( crop_rows )
     
     
 ## ======================= ##
 ##
-def main_loop( image_path ):
+def main_loop( data_path ):
     global screen
 
-    image = cv2.imread( image_path, cv2.IMREAD_COLOR )
+    data = loading.load_dataset( data_path )
+    full_images = data[ data[ "is_cropped" ] == False ]
+    first_row = full_images[ 0 ]
+    
+    load_row( data, first_row )
+    
+    image = cv2.imread( first_row[ "image" ].decode('UTF-8'), cv2.IMREAD_COLOR )
     screen = numpy.zeros( image.shape, numpy.uint8 )
     
     cv2.namedWindow( 'Crops labeling' )
