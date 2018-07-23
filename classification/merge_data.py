f4b8de6bea77fe2dfdee68e2e8ed29373d573080
Witek <nieznany.sprawiciel@gmail.com>
2018-07-23 12:32:24 +0200

    merge script continue

--- classification/merge_data.py
+++ classification/merge_data.py
@@ -26,17 +26,84 @@ def merge_datasets( data1, data2 ):
 
 ## ======================= ##
 ##
+def print_dtype_descr( type_desc ):
+
+    for column in type_desc:
+        print( column[ 1 ] + "      " + column[ 0 ] )
+   
+## ======================= ##
+##
+def print_dtype( data ):
+    print_dtype_descr( data.dtype.descr )
+   
+## ======================= ##
+##
+def is_label_in( label, type ):
+
+    for column in type.descr:
+        if column[ 0 ] == label[ 0 ]:
+            return True
+    return False
+    
+## ======================= ##
+##
+def compute_additional_labels( type1, type2 ):
+    
+    additional_labels = list()
+    
+    for column in type2.descr:
+        if not is_label_in( column, type1 ):
+            additional_labels.append( column )
+    
+    return additional_labels
+
+## ======================= ##
+##
+def build_index_mapping( data1, data2 ):
+
+    
+
+    
+## ======================= ##
+##
+def merge_datasets_new_metrics( data1, data2 ):
+    
+    print( "Computing dataset columns." )
+    additional_labels = compute_additional_labels( data1.dtype, data2.dtype )
+    
+    print( "Additional columns to add to dataset:" )
+    print_dtype_descr( additional_labels )
+    print( "" )
+    
+    print( "Creating new array" )
+    new_dtype = numpy.dtype( data1.dtype.descr + additional_labels )
+    extended_dataset = numpy.zeros( data1.shape, dtype=new_dtype )
+    
+    print( "Coping content to new array" )
+    for name in data1.dtype.names:
+        extended_dataset[ name ] = data1[ name ]
+        
+
+    
+    return None
+    
+    
+## ======================= ##
+##
 def run():
 
     data = loading.load_dataset( sys.argv[ 1 ] )
-    print( data.dtype )
-    data2 = loading.load_dataset( sys.argv[ 2 ] )
-    print( data2.dtype )
-
-    data = merge_datasets( data, data2 )
-    print( data.dtype )
+    print_dtype( data )
     
-    loading.save_binary( data, sys.argv[ 3 ] )
+    data2 = loading.load_dataset( sys.argv[ 2 ] )
+    print_dtype( data2 )
+
+    #data = merge_datasets( data, data2 )
+    #print( data.dtype )
+    
+    merge_datasets_new_metrics( data, data2 )
+    
+    #loading.save_binary( data, sys.argv[ 3 ] )
     
 
 if __name__ == "__main__":
